ee6fe337915915480dfa0d878a6b79622133cf84
Fabri <Fabrizioreyesr@gmail.com>
2024-04-11 10:03:32 +0200

    a

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,16 +6,17 @@ import './index.css'
 // import { CouterWithCustomHook } from './01-useState/CouterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   </React.StrictMode>,
 )
